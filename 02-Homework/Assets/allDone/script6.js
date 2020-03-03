d6cccc74a47c704bc384dfe7884ac53f3bb6042f
Marc Strong <marcstrong@yahoo.com>
2020-03-02 23:53:43 -0600

    update Readme

--- 02-Homework/Assets/allDone/script6.js
+++ 02-Homework/Assets/allDone/script6.js
@@ -16,6 +16,24 @@ if (result == "true") {
 else {
     displayResult.innerHTML = "Wrong!";
 }
+entry = '';
+function myFunction() {
+    // document.getElementById("myForm").addEventListener("onkeydown", function() {
+    //         var characters = '';
+    //         if (lowercase) {
+    //           characters += 'abcdefghijklmnopqrstuvwxyz';
+    //           console.log(characters);
+    //         }
+    // })
+    // var x = document.getElementById("myForm") 
+    // alert($(this).val())
+    // // alert(y);
+    // console.log(x)
+    // document.getElementById("demo").innerHTML = x;
+}
+
+var obj = { name: "John", age: 30, city: "New York" };
+var myJSON = JSON.stringify(obj);
 
 var value = ["aa", timeLeft]
 localStorage.setItem("testKey", JSON.stringify(value));
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # HW4-Code-Quiz
+Coding Quiz Challenge webpage presents
+1. Instructions, upon pressing putting starts the quiz
+2. Presents 5 questions, timed - starting at 75 seconds. If the answer is wrong, subtracts 15 seconds.
+3. When quiz is complete, presents you time and a form to complete to save your score
+4. When submiting form, view the highscores
