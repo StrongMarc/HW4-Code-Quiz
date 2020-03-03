b9bf4a21265b3daaaaa111c4f3884db3ea785288
Marc Strong <marcstrong@yahoo.com>
2020-03-03 01:27:28 -0600

    clear score

--- 02-Homework/Assets/Highscores/script7.js
+++ 02-Homework/Assets/Highscores/script7.js
@@ -12,6 +12,13 @@ document.getElementById("back").addEventListener("click", function() {
     backPage()
 })
 
+document.getElementById("clear").addEventListener("click", function() {
+    x = ""
+    timeLeft = ""
+    obj = {x , timeLeft }
+    finalEl.textContent = obj.x + "," + obj.timeLeft;
+})
+
 // function to go to question2.html
 function backPage() {
     window.location.assign("../allDone/allDone.html")
